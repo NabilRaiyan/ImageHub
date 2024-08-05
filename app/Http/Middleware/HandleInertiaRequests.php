a5187bdd98759a7ffcaa09c600d65e50fc811431
NabilRaiyan <rraiyan77@gmail.com>
2024-08-05 14:31:22 +0600

    creating package pricing card

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -31,6 +31,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            // adding csrf token
             'csrf_token' => csrf_token(),
             'auth' => [
                 'user' => $request->user(),
